73ebb957fd522b854c817d5f14acaec7f4d7a229
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-01-03 17:13:46 -0500

    python-p3-running-python-code/
    python-p3-running-python-code/

--- lib/testing/app_test.py
+++ lib/testing/app_test.py
@@ -5,24 +5,28 @@ import runpy
 import io
 import sys
 
-
 class TestAppPy:
-    # app.py
-
+    '''
+    app.py
+    '''
     def test_app_py_exists(self):
-        # exists in lib directory
-        assert path.exists("lib/app.py")
+        '''
+        exists in lib directory
+        '''
+        assert(path.exists("lib/app.py"))
 
     def test_app_py_runs(self):
-        # is executable
+        '''
+        is executable
+        '''
         runpy.run_path("lib/app.py")
 
     def test_prints_hello_world(self):
-        # prints "Hello World! Pass this test, please."
-        print("Hello World! Pass this test, please.")
+        '''
+        prints "Hello World! Pass this test, please."
+        '''
         captured_out = io.StringIO()
         sys.stdout = captured_out
         runpy.run_path("lib/app.py")
         sys.stdout = sys.__stdout__
-        assert captured_out.getvalue() == "Hello World! Pass this test, please.\n"
-
+        assert(captured_out.getvalue() == "Hello World! Pass this test, please.\n")
