cd44b61bef8f222851b3b6e794417be44fc7fa23
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-01-03 16:46:58 -0500

    python-p3-running-python-code
    python-p3-running-python-code

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-print("Hello world!");
-print("Hello sun!");
-print("Hello sky");
-
-print("Hello world!", end=" ")
-print("Hello sun!", end="!! ")
-print("Hello sky!", end="!!!\n")
-
-#=> Hello world! Hello sun!! Hello sku!!! 
-
-
-
-      
+print("Hello world!")
